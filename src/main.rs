f2df3e6c8b3c9bf5b6afb027a11c2f5c24111c8b
FR13ND-ly <57838113+FR13ND-ly@users.noreply.github.com>
2023-10-06 21:24:11 +0300

    change origins

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(
                 Cors::default()
-                    .allowed_origin("http://localhost:8081")
+                    .allow_any_origin()
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
